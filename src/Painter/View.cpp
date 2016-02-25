8f97408bec7c2ec2bef187826462ee695eae8d54
Piotr Krupa <piotrkrupa06@gmail.com>
2016-02-25 02:51:15 +0100

    Improvement scaling view

--- src/Painter/View.cpp
+++ src/Painter/View.cpp
@@ -56,10 +56,26 @@ void CView::ResetView()
 	m_iOffset = glm::ivec2(0, 0);
 }
 
+float _fScale;
+
 void CView::SetScale(float y)
 {
+	if (y > 0 && m_fScale >= 20)
+		return;
+
 	m_fScale += y / 10 * m_fScale;
 
-	m_iOffset.x -= y * -(CClient::Get()->GetRenderer()->GetWindowSize().x * 1 / 3 - CClient::Get()->GetCursor()->GetPos().x)/4;
-	m_iOffset.y -= y * -(CClient::Get()->GetRenderer()->GetWindowSize().y * 1 / 3 - CClient::Get()->GetCursor()->GetPos().y)/4;
+	if (m_fScale > 17.5)
+		_fScale = 0.5f;
+	else if (m_fScale > 15)
+		_fScale = 0.8f;
+	else if (m_fScale > 10)
+		_fScale = 1.f;
+	else if (m_fScale > 5)
+		_fScale = 1.5f;
+	else
+		_fScale = 1 + (21 - m_fScale)/10;
+		
+	m_iOffset.x -= y * -(CClient::Get()->GetRenderer()->GetWindowSize().x / 3 - CClient::Get()->GetCursor()->GetPos().x) / _fScale;
+	m_iOffset.y -= y * -(CClient::Get()->GetRenderer()->GetWindowSize().y / 3 - CClient::Get()->GetCursor()->GetPos().y) / _fScale;
 }
